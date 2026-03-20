3833212cd67be935127f5358bf25e760985168a0
LeggyBoiTim <105779117+LeggyBoiTim@users.noreply.github.com>
2026-03-20 15:04:26 +0100

    Created project structure

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AdController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/ads', [AdController::class, 'index'])->name('ads.index');
+    Route::redirect('/', '/ads');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/ads', [AdController::class, 'index'])->name('ads.index');
+    Route::redirect('/', '/ads');
 });
