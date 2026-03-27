75ec9aa0e55a209186795d39174ce8c4ea620f93
LeggyBoiTim <105779117+LeggyBoiTim@users.noreply.github.com>
2026-03-27 15:32:29 +0100

    Updated migrations, added ad_category pivot table

--- database/migrations/2026_03_20_135207_create_messages_table.php
+++ database/migrations/2026_03_20_135207_create_messages_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Conversation;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Conversation::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->text('body');
             $table->timestamps();
         });
     }
--- database/migrations/2026_03_27_142626_create_ad_category_table.php
+++ database/migrations/2026_03_27_142626_create_ad_category_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ads', function (Blueprint $table) {
+        Schema::create('ad_category', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
-            $table->string('title');
-            $table->text('description');
-            $table->float('price', 2);
-            $table->timestamps();
+            $table->foreignId('ad_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
         });
     }
 
@@ -27,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('ads');
+        Schema::dropIfExists('ad_category');
     }
 };
